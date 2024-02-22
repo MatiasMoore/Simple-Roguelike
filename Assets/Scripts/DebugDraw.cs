aa52c4000403bbe8c16138061d7af268515503b4
MatiasMoore <nikita.birukov.03@mail.ru>
2024-02-22 22:26:17 +0300

    feat(DebugDraw): created class for gizmo drawing

---
+++ Assets/Scripts/DebugDraw.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DebugDraw
+{
+    public static void DrawRectangle(Vector3 upperLeft, Vector3 upperRight, Vector3 lowerRight, Vector3 lowerLeft, Color color)
+    {
+        Gizmos.color = color;
+        Gizmos.DrawLineStrip(new Vector3[] { upperLeft, upperRight, lowerRight, lowerLeft }, true);
+    }
+
+    public static void DrawCross(Vector2 pos, float length, Color color)
+    {
+        Gizmos.color = color;
+        Gizmos.DrawLine(pos + new Vector2(-length, 0), pos + new Vector2(length, 0));
+        Gizmos.DrawLine(pos + new Vector2(0, -length), pos + new Vector2(0, length));
+    }
+
+    public static void DrawCell(Vector3 pos, float size, Color color)
+    {
+        Gizmos.color = color;
+        Gizmos.DrawWireCube(pos, new Vector3(size, size, 0));
+    }
+
+    public static void DrawLine(Vector3 start, Vector3 end, Color color)
+    {
+        Gizmos.color = color;
+        Gizmos.DrawLine(start, end);
+    }
+}
