3bcabf0242f4cbe7a3df46c96deaaee7c21b181c
MatiasMoore <nikita.birukov.03@mail.ru>
2024-02-23 13:47:21 +0300

    feat(LevelGeneratorMono): better editor interface for level generator

--- Assets/Scripts/LevelGeneration/LevelGeneratorMono.cs
+++ Assets/Scripts/LevelGeneration/LevelGeneratorMono.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 
 public class LevelGeneratorMono : MonoBehaviour
 {
+    [Header("Press this to generate a level!")]
+    [SerializeField]
+    private bool _generateNewLevel = false;
+
+    [Header("Generation settings")]
     [SerializeField]
     private int _initialWidth = 50;
     [SerializeField]
@@ -20,22 +25,33 @@ public class LevelGeneratorMono : MonoBehaviour
     [SerializeField]
     private bool _randomiseSeed = false;
 
+    [Header("Debug draw settings")]
     [SerializeField]
-    private bool _debugDrawRoomEdges = true;
+    private bool _drawRoomEdges = true;
     [SerializeField]
-    private bool _debugDrawRoomCenter = false;
+    private bool _drawRoomCenter = false;
     [SerializeField]
-    private bool _debugDrawAllTiles = false;
+    private bool _drawAllTiles = false;
     [SerializeField]
-    private bool _debugDrawPerimeterTiles = false;
+    private bool _drawPerimeterTiles = false;
     [SerializeField]
-    private bool _debugDrawRoomConnections = false;
+    private bool _drawCenterConnections = false;
     [SerializeField]
-    private bool _debugDrawRoomCorridors = true;
+    private bool _drawRoomConnections = true;
 
     private LevelGenerator _generator;
 
-    private void OnEnable()
+
+    private void OnValidate()
+    {
+        if (_generateNewLevel)
+        {
+            GenerateNewLevel();
+            _generateNewLevel = false;
+        }
+    }
+
+    private void GenerateNewLevel()
     {
         if (_randomiseSeed) 
             _seed = Random.Range(int.MinValue, int.MaxValue);
@@ -44,9 +60,9 @@ public class LevelGeneratorMono : MonoBehaviour
         _generator.GenerateNewLevel();
     }
 
-    void OnDrawGizmosSelected()
+    private void OnDrawGizmosSelected()
     {
         if (_generator != null)
-            _generator.DebugDrawLevel(_debugDrawRoomEdges, _debugDrawRoomCenter, _debugDrawAllTiles, _debugDrawPerimeterTiles, _debugDrawRoomConnections, _debugDrawRoomCorridors);
+            _generator.DebugDrawLevel(_drawRoomEdges, _drawRoomCenter, _drawAllTiles, _drawPerimeterTiles, _drawCenterConnections, _drawRoomConnections);
     }
 }
