c0e91271e8cf126573172807a61b99aabf43e94b
T-LORE <tlore007@gmail.com>
2024-02-24 04:28:39 +0300

    feat(player): add walk and watch animations to player

--- Assets/Scripts/Objects/Pedestrians/Player/PlayerMovement.cs
+++ Assets/Scripts/Objects/Pedestrians/Player/PlayerMovement.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
 public class PlayerMovement : Pedestrian
 {
     private Rigidbody2D _rigidbody;
 
     private InputSystem _inputSystem;
+
+    [SerializeField]
+    private Animator _animator;
+
+    private const string _speedX = "SpeedX";
+    private const string _speedY = "SpeedY";
+    private const string _cursorX = "CursorX";
+    private const string _cursorY = "CursorY";
     
     public void Init(InputSystem inputSystem)
     { 
@@ -19,6 +29,14 @@ public class PlayerMovement : Pedestrian
         if (_inputSystem != null)
         {
             Move(InputSystem.Movement);
+            _animator.SetFloat(_speedX, InputSystem.Movement.x);
+            _animator.SetFloat(_speedY, InputSystem.Movement.y);
+            
+            //translate cursor position to scene position
+            Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(InputSystem.CursorPosition);
+
+            _animator.SetFloat(_cursorX, cursorPosition.x - transform.position.x);
+            _animator.SetFloat(_cursorY, cursorPosition.y - transform.position.y);
         } 
     }
 
