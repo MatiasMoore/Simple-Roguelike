595452bd19fa5f6076da213282ad20b5a19daa16
T-LORE <tlore007@gmail.com>
2024-02-25 16:09:29 +0300

    feat(InputSystem): add cursor press check

--- Assets/Scripts/Objects/Pedestrians/Player/InputSystem.cs
+++ Assets/Scripts/Objects/Pedestrians/Player/InputSystem.cs
@@ -11,6 +11,8 @@ public class InputSystem : MonoBehaviour
     
     public static Vector2 CursorPosition;
 
+    public static bool IsCursorPressed;
+
     public static InputSystem Instance { get; private set; }
 
     private PlayerInput _playerInput;
@@ -21,7 +23,11 @@ public class InputSystem : MonoBehaviour
 
     private InputAction _cursorClick;
 
+    private InputAction _cursorRelease;
+
     public UnityAction<Vector2> CursorClickEvent;
+
+    public UnityAction<Vector2> CursorReleaseEvent;
 
     [SerializeField]
     private bool _isDebugOn;
@@ -33,12 +39,12 @@ public class InputSystem : MonoBehaviour
         _playerInput = GetComponent<PlayerInput>();
 
         _moveAction = _playerInput.actions["Move"];
-
         _cursorPosition = _playerInput.actions["CursorPosition"];
-
         _cursorClick = _playerInput.actions["CursorClick"];
+        _cursorRelease = _playerInput.actions["CursorRelease"];
 
         _cursorClick.performed += (var) => OnCursorClick();
+        _cursorRelease.performed += (var) => OnCursorRelease();
 
         Instance = this;
     }
@@ -61,6 +67,15 @@ public class InputSystem : MonoBehaviour
         if (_isDebugOn)
             Debug.Log("Cursor click");
         CursorClickEvent?.Invoke(CursorPosition);
-    }   
+        IsCursorPressed = true;
+    }  
+    
+    public void OnCursorRelease()
+    {
+        if (_isDebugOn)
+            Debug.Log("Cursor release");
+        CursorReleaseEvent?.Invoke(CursorPosition);
+        IsCursorPressed = false;
+    }
 
 }
