ec1c9c25918bf924140162a67fd763c18fce51dd
T-LORE <tlore007@gmail.com>
2024-02-25 17:55:27 +0300

    feat(ak-47): now ak is a rifle

---
+++ Assets/Scripts/Weapons/ak-47/Rifle.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rifle : Weapon
+{
+    [SerializeField]
+    public RifleData Data;
+
+    private Vector3 _localScale;
+
+    [SerializeField]
+    private Transform _projectileSpawnPoint;
+
+    private float _timer;
+    private bool _isReadyToFire;
+    public override void Init()
+    {
+        Bullet bullet = Data.ProjectilePrefab.GetComponent<Bullet>();
+        bullet.SetAccelerationTime(0);
+        bullet.SetDecelerationTime(0);
+        bullet.SetChangeDirectionTime(0);
+        bullet.SetMaxSpeed(Data.ProjectileSpeed);
+        bullet.SetAliveTime(Data.ProjectileLifeTime);
+        bullet.SetDamage(Data.Damage);
+        gameObject.SetActive(true);
+        _localScale = transform.localScale;     
+        _isReadyToFire = true;
+    }
+    public override void Enter()
+    {
+        Debug.Log($"Weapon {transform.name} enter.");
+        Fire();
+
+    }
+    public override void RotateWeaponToPoint(Vector2 direction)
+    {
+        Vector2 directionToCursor = (direction - (Vector2)transform.position).normalized;
+        float angle = Mathf.Atan2(directionToCursor.y, directionToCursor.x) * Mathf.Rad2Deg;
+
+        if (angle > 90 || angle < -90)
+        {
+            transform.localScale = new Vector3(_localScale.x, -_localScale.y, _localScale.z);
+        }
+        else
+        {
+            transform.localScale = new Vector3(_localScale.x, _localScale.y, _localScale.z);
+        }
+
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+
+    private void Fire()
+    {
+        if (_isReadyToFire)
+        {
+            Instantiate(Data.ProjectilePrefab, _projectileSpawnPoint.position, transform.rotation);
+            _isReadyToFire = false;
+            _timer = Data.FireRate;
+        }
+    }
+
+    private void Update()
+    {
+        if (_timer > 0)
+        {
+            _timer -= Time.deltaTime;
+        }
+        else
+        {
+            _isReadyToFire = true;
+            _timer = 0;
+        }
+
+    }
+
+    public override void Deinit()
+    {
+        gameObject.SetActive(false);
+    }
+}
