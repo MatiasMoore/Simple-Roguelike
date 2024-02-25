4299dfb0fe3529182d18a33247ec4a36bb8410ee
T-LORE <tlore007@gmail.com>
2024-02-25 15:36:01 +0300

    feat(PlayerWeaponController): add player weapon controller

---
+++ Assets/Scripts/Objects/Pedestrians/Player/PlayerWeaponController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerWeaponController : MonoBehaviour
+{
+    [SerializeField]
+    private Weapon _weapon;
+
+    public void Init(InputSystem inputSystem)
+    {
+        InputSystem.Instance.CursorClickEvent.AddListener(Fire);
+        SetActiveWeapon(_weapon);
+    }
+
+    public void SetActiveWeapon(Weapon weapon)
+    {
+        _weapon = weapon;
+        _weapon.Init();
+    }
+
+    private void Update()
+    {
+        _weapon.RotateWeaponToPoint(InputSystem.CursorPosition);
+    }
+
+    private void Fire(Vector2 direction)
+    {
+        Debug.Log($"{transform.name} fired to {direction}!");
+        _weapon.Enter();
+
+    }
+
+}
