1203292a432b5f3a8f64a24321e63cac371743c6
T-LORE <tlore007@gmail.com>
2024-03-02 03:55:22 +0300

    feat(PlayerWeaponController): now player can have many weapons

--- Assets/Scripts/Objects/Pedestrians/Player/PlayerWeaponController.cs
+++ Assets/Scripts/Objects/Pedestrians/Player/PlayerWeaponController.cs
@@ -6,40 +6,83 @@ using UnityEngine.Events;
 public class PlayerWeaponController : MonoBehaviour
 {
     [SerializeField]
-    private Weapon _weapon;
+    private List<Weapon> _weapons;
+
+    private Weapon _currentWeapon;
 
     public void Init(InputSystem inputSystem)
     {
-        SetActiveWeapon(_weapon);
+        if (_weapons.Count == 0)
+        {
+            Debug.LogError("No weapons found!");
+            return;
+        }
+        SetActiveWeapon(_weapons[0]);
+
         inputSystem.ReloadEvent += Reload;
+        inputSystem.ChooseNextWeapon += ChooseNextWeapon;
+        inputSystem.ChoosePreviousWeapon += ChoosePreviousWeapon;
     }
 
     public void SetActiveWeapon(Weapon weapon)
     {
-        _weapon = weapon;
-        _weapon.Init();
+        _currentWeapon = weapon;
+        _currentWeapon.Init();
     }
 
     private void Update()
     {
         if (InputSystem.IsCursorPressed)
             Fire(InputSystem.CursorPosition);
-        _weapon.RotateWeaponToPoint(InputSystem.CursorPosition);
+        _currentWeapon.RotateWeaponToPoint(InputSystem.CursorPosition);
     }
 
     private void Fire(Vector2 direction)
     {
         Debug.Log($"{transform.name} fired to {direction}!");
-        _weapon.Enter();
+        _currentWeapon.Enter();
 
     }
 
     private void Reload()
     {
-        if (_weapon is Rifle)
+        if (_currentWeapon is Rifle)
         {
-            (_weapon as Rifle).Reload();
+            (_currentWeapon as Rifle).Reload();
         }        
     }
 
+    private void ChooseWeapon(int i)
+    {
+        // if this weapon already choosed
+        if (_weapons[i] == _currentWeapon)
+        {
+            return;
+        }
+            
+       _currentWeapon.Deinit();
+        SetActiveWeapon(_weapons[i]);
+    }
+
+    private void ChooseNextWeapon()
+    {
+        int index = _weapons.IndexOf(_currentWeapon);
+        index++;
+        if (index >= _weapons.Count)
+        {
+            index = 0;
+        }
+        ChooseWeapon(index);
+    }
+
+    private void ChoosePreviousWeapon()
+    {
+        int index = _weapons.IndexOf(_currentWeapon);
+        index--;
+        if (index < 0)
+        {
+            index = _weapons.Count - 1;
+        }
+        ChooseWeapon(index);
+    }
 }
