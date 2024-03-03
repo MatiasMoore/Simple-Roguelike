2f45d3c780e6b1d45db1233427430a90d95357ba
T-LORE <tlore007@gmail.com>
2024-03-03 20:55:12 +0300

    feat(CollisionListener): added collision listener

---
+++ Assets/Scripts/CombatAi/MovementAI/CollisionListener.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class CollisionListener : MonoBehaviour
+{
+    public UnityAction<Collider2D> OnTriggerEnter;
+
+    public UnityAction<Collider2D> OnTriggerExit;
+
+    private void Start()
+    {
+        GetComponent<CircleCollider2D>().isTrigger = true;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        OnTriggerEnter?.Invoke(other);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        OnTriggerExit?.Invoke(collision);
+    }
+
+    public void SetRadius(float radius)
+    {
+        GetComponent<CircleCollider2D>().radius = radius;
+    }
+}
